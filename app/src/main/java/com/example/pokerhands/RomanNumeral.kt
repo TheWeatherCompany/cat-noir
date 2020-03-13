3defee945d9bf9893bb1519336eb06a35cffed82
Steve Ragle <steve.ragle@weather.com>
2020-03-13 12:16:09 -0400

    testing if I can push again again

--- app/src/main/java/com/example/pokerhands/RomanNumeral.kt
+++ app/src/main/java/com/example/pokerhands/RomanNumeral.kt
@@ -12,8 +12,14 @@ Roman Numerals
 Difficulty - Easy.
 
 Problem Description
-The Romans were a clever bunch. They conquered most of Europe and ruled it for hundreds of years. They invented concrete and straight roads and even bikinis [1]. One thing they never discovered though was the number zero. This made writing and dating extensive histories of their exploits slightly more challenging, but the system of numbers they came up with is still in use today. For example the BBC uses Roman numerals to date their programmes.
-The Romans wrote numbers using letters : I, V, X, L, C, D, M. (notice these letters have lots of straight lines and are hence easy to hack into stone tablets)
+The Romans were a clever bunch. They conquered most of Europe and ruled it for hundreds of years.
+They invented concrete and straight roads and even bikinis [1]. One thing they never discovered
+though was the number zero. This made writing and dating extensive histories of their exploits
+slightly more challenging, but the system of numbers they came up with is still in use today.
+For example the BBC uses Roman numerals to date their programmes.
+
+The Romans wrote numbers using letters : I, V, X, L, C, D, M. (notice these letters have lots of
+straight lines and are hence easy to hack into stone tablets)
 
 Part I
 The Kata says you should write a function to convert from normal numbers to Roman Numerals: eg
