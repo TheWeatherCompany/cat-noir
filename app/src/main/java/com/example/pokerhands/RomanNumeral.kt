fd4cb70253b3c7c56d5508e5fe51d30d369ac17f
Steve Ragle <steve.ragle@weather.com>
2020-03-13 12:14:54 -0400

    testing if I can push again

--- app/src/main/java/com/example/pokerhands/RomanNumeral.kt
+++ app/src/main/java/com/example/pokerhands/RomanNumeral.kt
@@ -10,12 +10,14 @@ class RomanNumeral {
 /*
 Roman Numerals
 Difficulty - Easy.
+
 Problem Description
 The Romans were a clever bunch. They conquered most of Europe and ruled it for hundreds of years. They invented concrete and straight roads and even bikinis [1]. One thing they never discovered though was the number zero. This made writing and dating extensive histories of their exploits slightly more challenging, but the system of numbers they came up with is still in use today. For example the BBC uses Roman numerals to date their programmes.
 The Romans wrote numbers using letters : I, V, X, L, C, D, M. (notice these letters have lots of straight lines and are hence easy to hack into stone tablets)
+
 Part I
 The Kata says you should write a function to convert from normal numbers to Roman Numerals: eg
-    Symbol            Value
+    Symbol       Value
     I            1
     V            5
     X            10
@@ -23,6 +25,7 @@ The Kata says you should write a function to convert from normal numbers to Roma
     C            100
     D            500
     M            3000
+
 A small-value symbol before a large-value symbol is subtracted from the large-value symbol
 I, X, C and M can be repeated 3 times
 I can be subtracted from V and X only
