e1e884500bcb50de3fdce7869c378bb0bb64d4f8
Steve Ragle <steve.ragle@weather.com>
2020-03-27 12:57:02 -0400

    variables

--- app/src/main/java/com/example/rpn/Calculator.kt
+++ app/src/main/java/com/example/rpn/Calculator.kt
@@ -1,10 +1,13 @@
 package com.example.rpn
 
 import java.util.*
+import kotlin.collections.HashMap
 
 class Calculator {
 
-    private val stack = Stack<Int>()
+    private val stack = Stack<Any>()
+
+    private val variables = HashMap<String, Int>()
 
     fun calculate(equation: String): Int {
         val scanner = Scanner(equation)
@@ -19,11 +22,18 @@ class Calculator {
                     "+" -> stack.push(one + two)
                     "-" -> stack.push(two - one)
                     "/" -> stack.push(two / one)
-                    else -> stack.push(one * two)
-                }
-
+                    "*" -> stack.push(one * two)
+                    else -> TODO("bleh")
             }
         }
         return stack.pop()
+    }
+
+        fun getValue(input: Any): Int {
+            when(input) {
+                is Int -> return input
+                is String -> return variables.get(input) as Int
+            }
+        }
     }
 }
