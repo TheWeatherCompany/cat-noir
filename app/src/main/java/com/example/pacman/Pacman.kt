6fca8576a5fe697f90d3097480acf9eb6ffe4cfe
Steve Ragle <steve.ragle@weather.com>
2020-04-10 12:55:43 -0400

    Steve

--- app/src/main/java/com/example/pacman/Pacman.kt
+++ app/src/main/java/com/example/pacman/Pacman.kt
@@ -2,11 +2,20 @@ package com.example.pacman
 
 class Pacman(width:Int,height:Int) {
 
-    private val board : Array<Array<Char>> = arrayOf(height, { arrayOf(width, {'.'}) })
+    private var board = arrayOf<Array<Char>>()
 
+    init {
+        for (i in 0 until height) {
+            var array = arrayOf<Char>()
+            for (j in 0 until width) {
+                array += '.'
+            }
+            board += array
+        }
+    }
 
     fun currentState():String{
-        var output = StringBuffer()
+        val output = StringBuffer()
         for (row in board) {
             for (cell in row) {
                 output.append(cell)
